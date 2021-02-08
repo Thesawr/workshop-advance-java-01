3b94da10c8f95e953bc37ad8d9557d22fe1cfb6f
KBANKNET\sarun.w <sarun.w@kbtg.tech>
2021-02-08 11:32:39 +0700

    create buffer

--- src/main/java/AdvanceJava/workshop01/CircularBuffer.java
+++ src/main/java/AdvanceJava/workshop01/CircularBuffer.java
@@ -1,4 +1,19 @@
 
 public class CircularBuffer {
-
+	
+	private int cap;
+	private String[] buff;
+	private int writePointer;
+	private int readPointer;
+	
+	//Default create
+	public void create() {
+		cap = 10;
+		buff = new String[cap];
+	}
+	
+	public void create(int size) {
+		cap = size;
+		buff = new String[cap];
+	}
 }
